8a2df0c88337c42fec503b3b5ccc92bd0ed9ad59
david <mrminecraft00@gmail.com>
2024-01-24 19:32:58 +0100

    added documentation

--- server/src/main/java/net/thenextlvl/passprotect/server/hashing/PBKDF2.java
+++ server/src/main/java/net/thenextlvl/passprotect/server/hashing/PBKDF2.java
@@ -6,6 +6,10 @@ import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
+/**
+ * The PBKDF2 class represents a password-based key derivation function 2.
+ * It provides methods to hash passwords using the PBKDF2 algorithm.
+ */
 public record PBKDF2(
         char[] password,
         byte[] salt,
@@ -28,6 +32,13 @@ public record PBKDF2(
         this(password.toCharArray(), salt, iterations, length);
     }
 
+    /**
+     * Hashes the password using the PBKDF2 algorithm.
+     *
+     * @return the hashed password as a byte array
+     * @throws InvalidKeySpecException  if the provided key specification is invalid
+     * @throws NoSuchAlgorithmException if the algorithm used for key generation is not found
+     */
     public byte[] hashBytes() throws InvalidKeySpecException, NoSuchAlgorithmException {
         var algorithm = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
         var spec = new PBEKeySpec(password, salt, iterations, length);
